e20a34a6c3e28d76c9f1bfddd4f3b70ad9b44965
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2024-11-03 22:20:56 +0900

    write our first program & structure of a go file

---
+++ main.go
@@ -0,0 +1,14 @@
+// main file
+
+package main // all code must belong to a package
+
+// we need to tell go where is the entry point of the program
+
+import "fmt"
+
+// import fmt package
+
+func main() {
+	fmt.Println("Welcome to our conference booking application!")
+	fmt.Println("Get your tickets here to attend")
+}
