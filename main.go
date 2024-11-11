fc8be985f9174d2172f8481ec169becf0fbb2aa1
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2024-11-11 17:27:48 +0900

    data types in go

--- main.go
+++ main.go
@@ -9,11 +9,22 @@ import "fmt"
 // import fmt package
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	var conferenceName string = "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets int = 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend!")
+
+	var userName string = ""
+	var userTicket int = 0
+	// ask user for their name
+
+	userName = "Tom"
+	userTicket = 2
+
+	fmt.Printf("User %v booked %v tickets.\n", userName, userTicket)
 }
